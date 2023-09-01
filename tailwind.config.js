e8e58e672cfb6c6160bd9fc57e8c31744591739f
danyal <danyallsty04@gmail.com>
2023-09-01 02:20:00 -0700

    tailwind form plugin added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["index.html","./js/.{js}"],
+  content: ["index.html","./src/**/*.js"],
   theme: {
     extend: {},
   },
